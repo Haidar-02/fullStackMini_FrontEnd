914c210600a61b10eefad53c10fe60d9d309265a
HaidarMestrah <10120319@mu.edu.lb>
2023-07-21 09:51:48 +0300

    applied js functionalities to form

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,64 @@
 pages = {};
+pages.page_index = () => {
+  const registerForm = document.getElementById("registerForm");
+  const passwordField = document.getElementById("Password");
+  const confirmPasswordField = document.getElementById("ConfirmPassword");
+  const showPassCheckbox = document.getElementById("ShowPassword");
 
-pages.page_index = () => {};
+  showPassCheckbox.addEventListener("change", function () {
+    const showPassword = showPassCheckbox.checked;
+    passwordField.type = showPassword ? "text" : "password";
+    confirmPasswordField.type = showPassword ? "text" : "password";
+  });
+  confirmPasswordField.addEventListener("input", function () {
+    if (passwordField.value !== confirmPasswordField.value) {
+      confirmPasswordField.style.border = "1px solid red";
+      return;
+    } else {
+      confirmPasswordField.style.border = "none";
+    }
+  });
+  passwordField.addEventListener("input", function () {
+    if (passwordField.value !== confirmPasswordField.value) {
+      confirmPasswordField.style.border = "1px solid red";
+      return;
+    } else {
+      confirmPasswordField.style.border = "none";
+    }
+  });
+
+  registerForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const firstName = document.getElementById("FirstName").value;
+    const lastName = document.getElementById("LastName").value;
+    const userName = document.getElementById("Username").value;
+    const password = passwordField.value;
+    const confirmPassword = confirmPasswordField.value;
+
+    if (password !== confirmPassword) {
+      confirmPasswordField.style.border = "1px solid red";
+      return;
+    } else {
+      confirmPasswordField.style.border = "none";
+    }
+
+    const formData = new FormData(registerForm);
+
+    fetch("register.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert(data.message);
+        if (data.success) {
+          window.location.href = "signin.html";
+        }
+      })
+      .catch((error) => console.error(error));
+  });
+};
 
 pages.page_signin = () => {};
 
